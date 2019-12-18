ad9baa215b152f2aea20ec9728db541761682009
Pooja Upadhyay <upooja2290@gmail.com>
2019-12-17 22:23:12 -0600

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         if (nums == null) {
-      return null;
-    }
-    int[] twoNum = new int[2];
-    //create a hashmap that will store key as the num[i] and value as index
-    HashMap<Integer, Integer> table = new HashMap<Integer, Integer>();
-    
-    int index = 0;
-    for (int i = 0; i < nums.length; i++) {
-    
-    // calculate the difference between target and num[i]
-      int value = target - nums[i];
-      
-      //if hashmap table contains the value then we found the solution
-      //put the last value of hashmap and the latest i value in the result array i.e twoNum in our case
-      if (table.containsKey(value)) {
-        twoNum[index] = table.get(value);
-        index++;
-        twoNum[index] = i;
-        break;
-      }
-      table.put(nums[i], i);
-    }
-    return twoNum;
+        if (nums.length <= 0) {
+            return null;
+        }
+        int[] result = new int[2];
+        int index = 0;
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
+            if(hashmap.containsKey(value)) {
+                result[index] = hashmap.get(value);
+                index++;
+                result[index] = i;
+            }
+            hashmap.put(nums[i], i);
+        }
+        
+        return result;
     }
 }
