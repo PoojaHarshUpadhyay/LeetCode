35e9d1915436b11bba9a58fe811b88af703d6e60
Pooja Upadhyay <upooja2290@gmail.com>
2020-02-09 19:40:32 -0600

    Update TopologicalSortGraph.java

--- TopologicalSortGraph.java
+++ TopologicalSortGraph.java
@@ -1,30 +1,31 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+      int count = 0; 
+    
         int[] inDegree = new int[numCourses];
-        int count = 0; 
         for(int i = 0; i < prerequisites.length; i++) {
             inDegree[prerequisites[i][0]]++;
         }
         LinkedList<Integer> stack = new LinkedList<>();
-        for(int i = 0; i < inDegree.length; i++) {
+        for(int i= 0; i < inDegree.length; i++) {
             if(inDegree[i] == 0) {
                 stack.push(i);
             }
         }
-        
         while(!stack.isEmpty()) {
-            int curr = stack.pop();
+            int temp = stack.pop();
             count++;
             for(int i = 0; i < prerequisites.length; i++) {
-                if(prerequisites[i][1] == curr) {
+                if( prerequisites[i][1] == temp) {
                     inDegree[prerequisites[i][0]]--;
                     if(inDegree[prerequisites[i][0]] == 0) {
-                        stack.push(prerequisites[i][0]);
+                         stack.push(prerequisites[i][0]);
                     }
                 }
             }
         }
-        return count==numCourses;
+        
+      return count == numCourses;
     }
 }
 
