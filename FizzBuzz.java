3bde773f706c8ec20dceb8664248954788a48a23
Pooja Upadhyay <upooja2290@gmail.com>
2020-03-05 22:02:04 -0600

    412. Fizz Buzz

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> lst = new ArrayList<>();
+        if(n < 0) {
+            return lst;
+        }
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i %5 == 0) {
+                lst.add("FizzBuzz");
+            }
+            else if(i%3 == 0) {
+                lst.add("Fizz");
+            }
+            else if (i % 5 == 0) {
+                lst.add("Buzz");
+            } else {
+                lst.add(Integer.toString(i));
+            }
+        }
+        return lst;
+    }
+}
