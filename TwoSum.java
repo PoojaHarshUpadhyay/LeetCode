8bd5e79a72a4ab1c8405a0b8746dfdcae3072000
Pooja Upadhyay <upooja2290@gmail.com>
2020-03-11 21:59:09 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        if (nums.length <= 0) {
-            return null;
-        }
-        int[] result = new int[2];
-        int index = 0;
-        HashMap<Integer, Integer> hashmap = new HashMap<>();
+       if(nums.length < 0){
+           return null;
+       }
+        HashMap<Integer, Integer> map = new HashMap<>();
         
+        int[] resArray = new int[2];
         for(int i = 0; i < nums.length; i++) {
-            int value = target - nums[i];
-            if(hashmap.containsKey(value)) {
-                result[index] = hashmap.get(value);
-                index++;
-                result[index] = i;
+            int sum = target - nums[i];
+            if(map.containsKey(sum)) {
+                int index1 = map.get(sum);
+                resArray[0] = index1;
+                resArray[1] = i;
+                break;
             }
-            hashmap.put(nums[i], i);
+            map.put(nums[i], i);
         }
-        
-        return result;
+        return resArray;
     }
 }
