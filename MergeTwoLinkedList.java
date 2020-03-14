051d5e0c16ecf3c9e1c6a994d0e3c895e04bcf3b
Pooja Upadhyay <upooja2290@gmail.com>
2020-03-13 23:12:42 -0500

    Update MergeTwoLinkedList.java

--- MergeTwoLinkedList.java
+++ MergeTwoLinkedList.java
@@ -8,28 +8,28 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null) {
-            return null;
-        }
         ListNode dummy = new ListNode(0);
         ListNode l3 = dummy;
+        
         while(l1 != null && l2 != null) {
             if(l1.val < l2.val) {
-                dummy.next  = new ListNode(l1.val);
+                dummy.next = new ListNode(l1.val);
                 l1 = l1.next;
             } else {
-                dummy.next  = new ListNode(l2.val);
+                dummy.next = new ListNode(l2.val);
                 l2 = l2.next;
             }
-            dummy = dummy.next;
+            dummy = dummy.next; 
         }
+        
         while(l1 != null) {
-            dummy.next  = new ListNode(l1.val);
+            dummy.next = new ListNode(l1.val);
             l1 = l1.next;
             dummy = dummy.next;
         }
-        while (l2 != null) {
-            dummy.next  = new ListNode(l2.val);
+        
+        while(l2 != null) {
+            dummy.next = new ListNode(l2.val);
             l2 = l2.next;
             dummy = dummy.next;
         }
