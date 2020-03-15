33a2a2899fb98a7c8e4818fd640e644e76869e20
Pooja Upadhyay <upooja2290@gmail.com>
2020-03-15 10:01:56 -0400

    1. Two Sum

--- TwoSum.java
+++ TwoSum.java
@@ -3,19 +3,18 @@ class Solution {
        if(nums.length < 0){
            return null;
        }
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        int[] resArray = new int[2];
+      HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
         for(int i = 0; i < nums.length; i++) {
             int sum = target - nums[i];
             if(map.containsKey(sum)) {
-                int index1 = map.get(sum);
-                resArray[0] = index1;
-                resArray[1] = i;
-                break;
+                int val = map.get(sum);
+                result[0] = val;
+                result[1] = i;
             }
             map.put(nums[i], i);
         }
-        return resArray;
+        
+        return result;
     }
 }
