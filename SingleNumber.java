aa4473b266b03621e755daba74fc36c96626f8e6
Pooja Upadhyay <upooja2290@gmail.com>
2020-04-01 20:34:18 -0400

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,27 +1,30 @@
 class Solution {
     public int singleNumber(int[] nums) {
-          if (nums.length <= 0) {
-            return 0;
+        int res = 0; 
+        if(nums.length == 0) {
+            return 0; 
         }
-        int result = 0;
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+        if( nums.length == 1) {
+            return nums[0];
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
             int count = 1;
-            if (!hashMap.containsKey(nums[i])) {
-                hashMap.put(nums[i], count);
+            if(map.containsKey(nums[i])) {
+                count = map.get(nums[i]);
+                count = count+1;
+                map.put(nums[i], count);
             } else {
-                int newCount = hashMap.get(nums[i]);
-                newCount++;
-                hashMap.replace(nums[i], newCount);
+                map.put(nums[i], count);
             }
         }
-        for (Map.Entry<Integer, Integer> e : hashMap.entrySet()) {
-            if (e.getValue() == 1) {
-                result = e.getKey();
+        for(Integer key : map.keySet()) {
+            int val = map.get(key);
+            if(val == 1) {
+                res = key;
                 break;
             }
         }
-
-        return result;
+        return res;
     }
 }
