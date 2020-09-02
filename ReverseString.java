6eb137d2b4e9d125774174d0b1e94b8ff7e075f0
Pooja Upadhyay <upooja2290@gmail.com>
2020-09-01 21:52:18 -0500

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,13 +1,15 @@
 class Solution {
     public void reverseString(char[] s) {
-            int i = 0; 
-        int j = s.length-1;
-        while (i < j) {
-            char temp = s[j];
-            s[j] = s[i];
-            s[i] = temp;
-            j--;
+       int i = 0;
+        int j = s.length - 1;
+        
+        while(i < j) {
+         char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
             i++;
+            J--;
+            
         }
     }
 }
