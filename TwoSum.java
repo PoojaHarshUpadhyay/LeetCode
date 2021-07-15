3cef3fa3925d2c15a33bb663a114283a4218afb5
Pooja Upadhyay <upooja2290@gmail.com>
2021-07-14 21:20:21 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       if(nums.length < 0){
-           return null;
-       }
-      HashMap<Integer, Integer> map = new HashMap<>();
+      if(nums.length <= 0) {
+          return null; 
+      }
         int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             int sum = target - nums[i];
             if(map.containsKey(sum)) {
@@ -14,7 +14,6 @@ class Solution {
             }
             map.put(nums[i], i);
         }
-        
         return result;
     }
 }
