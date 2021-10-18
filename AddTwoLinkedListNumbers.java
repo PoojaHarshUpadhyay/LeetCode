d1f94e00d73cd16f209ef31b5b19e7829209ebbf
Pooja Upadhyay <upooja2290@gmail.com>
2021-10-18 12:53:53 -0500

    Update AddTwoLinkedListNumbers.java

--- AddTwoLinkedListNumbers.java
+++ AddTwoLinkedListNumbers.java
@@ -8,43 +8,49 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-          ListNode dummy = new ListNode(0);
+      ListNode dummy = new ListNode(0);
         ListNode l3 = dummy;
         int carry = 0;
-        //when both l1 and l2 are not null 
         while(l1 != null && l2 != null) {
-            int val = l1.val + l2.val  + carry;
-            int lastDigit = val %10;
-            carry = val /10;
-            ListNode newNode = new ListNode(lastDigit);
-            l3.next = newNode;
+            int sum = l1.val + l2.val + carry;
+            int lastDigit = sum % 10;
+            carry = sum / 10;
+            ListNode n = new ListNode(lastDigit);
+            l3.next = n;
             l3 = l3.next;
             l2 = l2.next;
             l1 = l1.next;
         }
-        while(l1 != null) {
-            int val = l1.val + carry;
-            int lastDigit = val%10;
-            carry = val /10;
-            ListNode newNode = new ListNode(lastDigit);
-            l3.next = newNode;
+        
+        while (l1 != null) {
+            int sum = l1.val + carry;
+            int lastDigit = sum % 10;
+            carry = sum / 10;
+
+            ListNode n = new ListNode(lastDigit);
+            l3.next = n;
             l3 = l3.next;
             l1 = l1.next;
         }
+        
         while(l2 != null) {
-            int val = l2.val + carry;
-            int lastDigit = val % 10; 
-            carry = val / 10;
-            ListNode newNode = new ListNode(lastDigit);
-            l3.next = newNode;
+            int sum = l2.val + carry;
+           int lastDigit = sum % 10;
+           carry = sum /10; 
+
+            ListNode n = new ListNode(lastDigit);
+            l3.next = n;
             l3 = l3.next;
             l2 = l2.next;
         }
+        
         if(carry != 0) {
-            ListNode newNode = new ListNode(carry);
-            l3.next = newNode;
+            ListNode n = new ListNode(carry);
+            l3.next = n;
             l3 = l3.next;
         }
+        
         return dummy.next;
+        
     }
 }
