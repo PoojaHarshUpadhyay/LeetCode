59ec8804e78d0a87302afaec0bc544648e758dc2
Pooja Upadhyay <upooja2290@gmail.com>
2021-11-19 18:21:58 -0600

    Update LongestPalindrome.java

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,49 +1,38 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int maxLen = 0; 
-        int start = 0; 
-        if(s == null || s.length() <=0) {
-            return "";
+    
+  public String longestPalindrome(String s) {
+    int n = s.length();
+      if(n < 0) {
+          return "";
+      }
+      
+      String result = s.substring(0, 1); //single caracter is palindrome
+      
+      for(int i = 0; i < n; i++) {
+          //even
+          String p1 = expandAroundCorner(s, i, i);
+          if(p1.length() > result.length()) {
+              result = p1;
+          }
+          
+          //odd
+          String p2 = expandAroundCorner(s, i, i+1);
+          if(p2.length() > result.length()) {
+              result = p2;
+          }
+      }
+      return result;
+  }
+    
+    private String expandAroundCorner(String s, int l, int r) {
+        while((l >= 0 && r < s.length()) && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
         }
-        int n = s.length();
-        boolean[][] matrix = new boolean[n][n];
-        
-        //fill i, i index of matrix
-        for(int i = 0; i < n; i++) {
-            matrix[i][i] = true;
-            start = i; 
-            maxLen = 1;
-        }
-        
-        //fill i, i+1 index of matrix
-        for(int i = 0; i < n-1; i++) {
-            if(s.charAt(i) == s.charAt(i+1)) {
-                matrix[i][i+1] = true;
-                start = i; 
-                maxLen = 2;
-            }
-        }
-        
-        //fill remaining matrix
-        for(int k = 3; k <= n; k++) {
-            //iterate through k row 
-            for(int i = 0; i < n-k+1; i++) {
-                int j = k + i -1;
-                //if palindrome
-                if((s.charAt(i) == s.charAt(j))
-                  && (matrix[i+1][j-1] == true)) {
-                    matrix[i][j] = true;
-                    if(k > maxLen) {
-                        start = i;
-                        maxLen = k;
-                    }
-                }
-            }
-        }
-        
-        return s.substring(start, maxLen+start);
-        
+        return s.substring(l+1, r);
     }
-}
 
-//https://www.youtube.com/watch?v=UflHuQj6MVA
+
+
+    
+}
