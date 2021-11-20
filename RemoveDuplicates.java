ba539e5154f89c9ddc9f455c9c8ae14e3e1cedbe
Pooja Upadhyay <upooja2290@gmail.com>
2021-11-20 16:53:48 -0600

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -14,3 +14,21 @@ class Solution {
         return j+1;
     }
 }
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+         int res = 0;
+        int i = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (!set.contains(nums[j])) {
+                set.add(nums[j]);
+                nums[i] = nums[j];
+                i++;
+                res++;
+            }
+        }
+        return res;
+    }
+}
